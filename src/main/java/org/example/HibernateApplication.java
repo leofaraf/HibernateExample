90bc6c782d9f375712d4a268efd84a630bb688f4
leonfarafonov <117378906+leonfarafonov@users.noreply.github.com>
2023-02-04 10:56:07 +0300

    Add

--- src/main/java/org/example/HibernateApplication.java
+++ src/main/java/org/example/HibernateApplication.java
@@ -9,8 +9,8 @@ public class HibernateApplication {
     public static void main(String[] args) {
         System.out.println("__DATABASE_PROGRAM__");
         DatabaseService service = new DatabaseService();
-        System.out.println("DELETE apl with have id 10");
-        service.removeApplicationWithId(13);
+
+
 
         service.closeFactory();
     }
--- src/main/java/org/example/service/DatabaseService.java
+++ src/main/java/org/example/service/DatabaseService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import jakarta.persistence.Query;
 import org.example.structure.Application;
+import org.example.structure.ApplicationDetails;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,6 +12,7 @@ public class DatabaseService {
     private final SessionFactory factory = new Configuration()
             .configure("hibernate.cfg.xml")
             .addAnnotatedClass(Application.class)
+            .addAnnotatedClass(ApplicationDetails.class)
             .buildSessionFactory();
 
     public void addApplication(Application application) {
@@ -21,10 +23,28 @@ public class DatabaseService {
         session.close();
     }
 
+    public void addApplicationDetails(ApplicationDetails details, Application application) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        details.setApplication(application);
+        session.persist(details);
+        transaction.commit();
+        session.close();
+    }
+
     public Application getApplicationWithId(int id) {
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
         Application application = session.get(Application.class, id);
+        transaction.commit();
+        session.close();
+        return application;
+    }
+
+    public ApplicationDetails getApplicationDetailsWithId(int id) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        ApplicationDetails application = session.get(ApplicationDetails.class, id);
         transaction.commit();
         session.close();
         return application;
@@ -62,6 +82,23 @@ public class DatabaseService {
     }
 
     public void removeApplication(Application application) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(application);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeApplicationDetailsWithId(int id) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        ApplicationDetails application = session.get(ApplicationDetails.class, id);
+        session.remove(application);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeApplicationDetails(ApplicationDetails application) {
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
         session.remove(application);
